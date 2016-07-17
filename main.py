5637cd65e93b8017c6f41ba017dc2567ffea8ab9
Arpit Gogia <arpitgogia@protonmail.com>
2016-07-17 15:13:49 +0530

    Extracted Data

---
+++ main.py
@@ -0,0 +1,15 @@
+import tensorflow 
+import scipy.io
+from sklearn.cross_validation import train_test_split
+import numpy
+
+train_data = scipy.io.loadmat('train_32x32.mat', variable_names='X').get('X')
+train_labels = scipy.io.loadmat('train_32x32.mat', variable_names='y').get('y')
+test_data = scipy.io.loadmat('test_32x32.mat', variable_names='X').get('X')
+test_labels = scipy.io.loadmat('test_32x32.mat', variable_names='y').get('y')
+
+shape_train = train_data.shape
+shape_test = test_data.shape
+
+print shape_train[3], "Images with", shape_train[0], "x", shape_train[0], "RGB grid"
+
