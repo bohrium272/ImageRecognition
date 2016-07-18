4a41425ab2fb08e6dc3ae14661ac1538f2f12e89
Arpit Gogia <arpitgogia@protonmail.com>
2016-07-18 18:26:11 +0530

    Changes

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ shape_test = test_data.shape
 print shape_train[3], "Images with", shape_train[0], "x", shape_train[0], "RGB grid"
 #==================NORMALISATION=============================================
 
-train_data = train_data.astype('float32') / 128.0 - 1
-test_data = test_data.astype('float32') / 128.0 - 1
+# train_data = train_data.astype('float32') / 128.0 - 1
+# test_data = test_data.astype('float32') / 128.0 - 1
 
 #============================================================================
 image_size = 32
@@ -38,7 +38,6 @@ height = 32
 channels = 3
 
 n_labels = 10
-droput = 0.76
 kernel_dimen = 5
 batch = 16
 depth = 16
@@ -47,73 +46,82 @@ hidden = 64
 learning_rate = 0.001
 
 def accuracy(predictions, labels):
-  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+    return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
 print train_labels.shape
 def reformat(dataset, labels):
-  dataset = dataset.reshape(
-    (-1, image_size, image_size, channels)).astype(np.float32)
-  labels = (np.arange(n_labels) == labels[:,:]).astype(np.float32)
-  return dataset, labels
+    dataset = dataset.reshape(
+        (-1, image_size, image_size, channels)).astype(np.float32)
+    labels = (np.arange(n_labels) == labels[:,:]).astype(np.float32)
+    return dataset, labels
 train_data, train_labels = reformat(train_data, train_labels)
 test_data, test_labels = reformat(test_data, test_labels)
 print train_labels.shape, test_labels.shape
-graph = tf.Graph()
+# graph = tf.Graph()
+# with graph.as_default():
+tf_train_dataset = tf.placeholder(tf.float32, shape=(batch, width, height, channels))
+tf_train_labels = tf.placeholder(tf.float32, shape=(batch, n_labels))
+tf_test_dataset = tf.constant(test_data)
 
-with graph.as_default():
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch, width, height, channels))
-    tf_train_labels = tf.placeholder(tf.float32, shape=(batch, n_labels))
-    tf_test_dataset = tf.constant(test_data)
+layer1_weights = tf.Variable(tf.truncated_normal([kernel_dimen, kernel_dimen, channels, depth], stddev=0.1))
+layer1_biases = tf.Variable(tf.zeros([depth]))
 
-    layer1_weights = tf.Variable(tf.truncated_normal([kernel_dimen, kernel_dimen, channels, depth], stddev=0.1))
-    layer1_biases = tf.Variable(tf.random_normal([depth], stddev=0.1))
+layer2_weights = tf.Variable(tf.truncated_normal([kernel_dimen, kernel_dimen, depth, depth], stddev=0.1))
+layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
 
-    layer2_weights = tf.Variable(tf.truncated_normal([kernel_dimen, kernel_dimen, depth, depth], stddev=0.1))
-    layer2_biases = tf.Variable(tf.random_normal([depth], stddev=0.1))
+layer3_weights = tf.Variable(tf.truncated_normal([image_size // 4 * image_size // 4 * depth, hidden], stddev=0.1))
+layer3_biases = tf.Variable(tf.constant(1.0, shape=[hidden]))
+
+layer4_weights = tf.Variable(tf.truncated_normal([hidden, n_labels], stddev=0.1))
+layer4_biases = tf.Variable(tf.constant(1.0, shape=[n_labels]))
+
+dropout = tf.placeholder(tf.float32)
+
+def model(data):
+    conv1 = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+    hidden1 = tf.nn.relu(conv1 + layer1_biases)
+
+    hidden2 = tf.nn.max_pool(hidden1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    conv2 = tf.nn.conv2d(hidden2, layer2_weights, [1, 1, 1, 1], padding='SAME')
+    hidden3 = tf.nn.relu(conv2 + layer2_biases)
+
+    hidden4 = tf.nn.max_pool(hidden3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    shape = hidden4.get_shape().as_list()
+
+    reshape = tf.reshape(hidden4, [-1, shape[1] * shape[2] * shape[3]])
+    hidden5 = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
     
-    layer3_weights = tf.Variable(tf.truncated_normal([image_size // 4 * image_size // 4 * depth, hidden], stddev=0.1))
-    layer3_biases = tf.Variable(tf.random_normal([hidden]))
-
-    layer4_weights = tf.Variable(tf.truncated_normal([hidden, n_labels], stddev=0.1))
-    layer4_biases = tf.Variable(tf.random_normal([n_labels], stddev=0.1))
-
-    dropout = tf.placeholder(tf.float32)
-
-    def model(data):
-        conv = tf.nn.conv2d(data, layer1_weights, [1, 2, 2, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer1_biases)
-        conv = tf.nn.conv2d(hidden, layer2_weights, [1, 2, 2, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer2_biases)
-        shape = hidden.get_shape().as_list()
-        reshape = tf.reshape(hidden, [-1, shape[1] * shape[2] * shape[3]])
-        hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
-        dropout_layer = tf.nn.dropout(hidden, dropout)
-        return tf.matmul(dropout_layer, layer4_weights) + layer4_biases
+    dropout_layer = tf.nn.dropout(hidden5, 0.75)
     
-    logits = model(tf_train_dataset)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+    return tf.matmul(dropout_layer, layer4_weights) + layer4_biases
 
-    optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+logits = model(tf_train_dataset)
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
 
-    train_prediction = tf.nn.softmax(logits)
-    test_prediction = tf.nn.softmax(model(tf_test_dataset))
+optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
 
-    num_steps = 1001
+train_prediction = tf.nn.softmax(logits)
+test_prediction = tf.nn.softmax(model(tf_test_dataset))
 
-with tf.Session(graph=graph) as session:
-  tf.initialize_all_variables().run()
-  print('Initialized')
+num_steps = 3000
+
+with tf.Session() as session:
+    tf.initialize_all_variables().run()
+    print('Initialized')
+    average = 0
+    for step in range(num_steps):
+        offset = (step * batch) % (train_labels.shape[0] - batch)
+        batch_data = train_data[offset:(offset + batch), :, :, :]
+        batch_labels = train_labels[offset:(offset + batch), :]
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels, dropout: 0.75}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if (step % 50 == 0):
+            print('Minibatch loss at step %d: %f' % (step, l))
+            accu = accuracy(predictions, batch_labels)
+            print('Minibatch accuracy: %.1f%%' % accu)
+            average += accu
+    print "Average Accuracy : ", (average / num_steps) * 100
   
-  for step in range(num_steps):
-    offset = (step * batch) % (train_labels.shape[0] - batch)
-    batch_data = train_data[offset:(offset + batch), :, :, :]
-    batch_labels = train_labels[offset:(offset + batch), :]
-    feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels, dropout:0.75}
-    _, l, predictions = session.run(
-      [optimizer, loss, train_prediction], feed_dict=feed_dict)
-    if (step % 50 == 0):
-      print('Minibatch loss at step %d: %f' % (step, l))
-      print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
-    #   print('Validation accuracy: %.1f%%' % accuracy(
-    #     valid_prediction.eval(), valid_labels))
-  print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+  # print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
