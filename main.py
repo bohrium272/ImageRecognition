7dea57f56eb955a9fd215b4b96776ee130ba184e
Arpit Gogia <arpitgogia@protonmail.com>
2016-07-19 19:07:39 +0530

    Output added

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ test_labels = scipy.io.loadmat('test_32x32.mat')['y']
 shape_train = train_data.shape
 shape_test = test_data.shape
 
-# temp_labels = train_labels.reshape(73257).tolist()
-# temp_labels = dict(Counter(temp_labels))
-# plt.bar(range(len(temp_labels)), temp_labels.values(), align='center')
-# plt.xticks(range(len(temp_labels)), temp_labels.keys())
-
-# temp_labels = test_labels.reshape(26032).tolist()
-# temp_labels = dict(Counter(temp_labels))
-# plt.bar(range(len(temp_labels)), temp_labels.values(), align='center', color='red')
-
-# plt.show()
+temp_labels = train_labels.reshape(73257).tolist()
+temp_labels = dict(Counter(temp_labels))
+plt.bar(range(len(temp_labels)), temp_labels.values(), align='center', label='Training Labels')
+plt.xticks(range(len(temp_labels)), temp_labels.keys())
+temp_labels = test_labels.reshape(26032).tolist()
+temp_labels = dict(Counter(temp_labels))
+plt.bar(range(len(temp_labels)), temp_labels.values(), align='center', color='red', label='Testing Labels')
+plt.legend()
+plt.xlabel('Class Labels')
+plt.ylabel('Frequency')
+plt.title('Frequency Distribution of Class Labels')
+plt.show()
 #============================================================================
 
 print shape_train[3], "Images with", shape_train[0], "x", shape_train[0], "RGB grid"
@@ -31,33 +33,6 @@ print shape_train[3], "Images with", shape_train[0], "x", shape_train[0], "RGB g
 train_data = train_data.astype('float32') / 128.0 - 1
 test_data = test_data.astype('float32') / 128.0 - 1
 
-def OnehotEndoding(Y):
-    Ytr=[]
-    for el in Y:
-        temp=np.zeros(10)
-        if el==10:
-            temp[0]=1
-        elif el==1:
-            temp[1]=1
-        elif el==2:
-            temp[2]=1
-        elif el==3:
-            temp[3]=1
-        elif el==4:
-            temp[4]=1
-        elif el==5:
-            temp[5]=1
-        elif el==6:
-            temp[6]=1
-        elif el==7:
-            temp[7]=1
-        elif el==8:
-            temp[8]=1
-        elif el==9:
-            temp[9]=1
-        Ytr.append(temp)
-        
-    return np.asarray(Ytr)
 def reformat(data, Y):
     xtrain = []
     trainLen = data.shape[3]
@@ -92,12 +67,7 @@ def reformat(data, Y):
 
 train_data, train_labels = reformat(train_data, train_labels)
 test_data, test_labels = reformat(test_data, test_labels)
-
-# train_labels = OnehotEndoding(train_labels)
-# test_labels = OnehotEndoding(test_labels)
-
-print train_data.shape
-
+print test_data.shape
 #============================================================================
 image_size = 32
 width = 32
@@ -162,8 +132,6 @@ test_prediction = tf.nn.softmax(model(tf_test_dataset))
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
-print train_labels.shape
-
 
 num_steps = 10000
 
@@ -177,10 +145,23 @@ with tf.Session() as session:
         batch_labels = train_labels[offset:(offset + batch), :]
         feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels, dropout: 0.75}
         _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        accu = accuracy(predictions, batch_labels)
         if (step % 50 == 0):
             print('Minibatch loss at step %d: %f' % (step, l))
-            accu = accuracy(predictions, batch_labels)
             print('Minibatch accuracy: %.1f%%' % accu)
-            average += accu
-    # print "Average Accuracy : ", (average / num_steps * 100) * 100
-    # print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
+        average += accu
+    print "Average Accuracy : ", (average / num_steps)
+    average = 0
+    for step in range(num_steps):
+        offset = (step * batch) % (test_labels.shape[0] - batch)
+        batch_data = test_data[offset:(offset + batch), :, :, :]
+        batch_labels = test_labels[offset:(offset + batch), :]
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels, dropout: 0.75}
+        _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+        accu = accuracy(predictions, batch_labels)
+        if (step % 50 == 0):
+            print('Minibatch loss at step %d: %f' % (step, l))
+            print('Minibatch accuracy: %.1f%%' % accu)
+        average += accu
+    print "Average Accuracy : ", (average / num_steps)
+    # session.run([optimizer, loss, test_prediction], feed_dict={tf_train_dataset : test_data, tf_train_labels : test_labels, dropout: 1.0})
